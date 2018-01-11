3d703564ef539859670352fd6a9ef4f781f17f1b
Andy <ahalterman0@gmail.com>
2018-01-11 08:18:18 -0500

    Add progress bar and per-iteration saving

--- train_ner_mod.py
+++ train_ner_mod.py
@@ -6,6 +6,9 @@ For more details, see the documentation:
 * Training: https://spacy.io/usage/training
 * NER: https://spacy.io/usage/linguistic-features#named-entities
 Compatible with: spaCy v2.0.0+
+
+Taken from https://github.com/explosion/spaCy/blob/master/examples/training/train_ner.py,
+and modified to accept JSON file input
 """
 from __future__ import unicode_literals, print_function
 
@@ -13,25 +16,23 @@ import plac
 import random
 from pathlib import Path
 import spacy
-
-
-# training data
-TRAIN_DATA = [
-    ('Who is Shaka Khan?', {
-        'entities': [(7, 17, 'PERSON')]
-    }),
-    ('I like London and Berlin.', {
-        'entities': [(7, 13, 'LOC'), (18, 24, 'LOC')]
-    })
-]
+import json
+from tqdm import tqdm
 
 
 @plac.annotations(
+    data=("JSON of training data", "option", "d", Path),
     model=("Model name. Defaults to blank 'en' model.", "option", "m", str),
     output_dir=("Optional output directory", "option", "o", Path),
     n_iter=("Number of training iterations", "option", "n", int))
-def main(model=None, output_dir=None, n_iter=100):
+def main(data, model=None, output_dir=None, n_iter=100):
     """Load the model, set up the pipeline and train the entity recognizer."""
+    with open(data, "r") as f:
+        TRAIN_DATA = json.load(f)
+    if output_dir is not None:
+        output_dir = Path(output_dir)
+        if not output_dir.exists():
+            output_dir.mkdir()
     if model is not None:
         nlp = spacy.load(model)  # load existing spaCy model
         print("Loaded model '%s'" % model)
@@ -52,15 +53,16 @@ def main(model=None, output_dir=None, n_iter=100):
     for _, annotations in TRAIN_DATA:
         for ent in annotations.get('entities'):
             ner.add_label(ent[2])
-
+    print("Starting training")
     # get names of other pipes to disable them during training
     other_pipes = [pipe for pipe in nlp.pipe_names if pipe != 'ner']
     with nlp.disable_pipes(*other_pipes):  # only train NER
         optimizer = nlp.begin_training()
         for itn in range(n_iter):
+            print("Iteration ", itn)
             random.shuffle(TRAIN_DATA)
             losses = {}
-            for text, annotations in TRAIN_DATA:
+            for text, annotations in tqdm(TRAIN_DATA):
                 nlp.update(
                     [text],  # batch of texts
                     [annotations],  # batch of annotations
@@ -68,28 +70,33 @@ def main(model=None, output_dir=None, n_iter=100):
                     sgd=optimizer,  # callable to update weights
                     losses=losses)
             print(losses)
+            nlp.to_disk(output_dir)
+
+    TEST_DATA = [
+        ('Who is Shaka Khan?', {
+            'entities': [(7, 17, 'PERSON')]
+        }),
+        ('I like London and Berlin.', {
+            'entities': [(7, 13, 'LOC'), (18, 24, 'LOC')]
+        })
+    ]
+
 
     # test the trained model
-    for text, _ in TRAIN_DATA:
+    for text, _ in TEST_DATA:
         doc = nlp(text)
         print('Entities', [(ent.text, ent.label_) for ent in doc.ents])
         print('Tokens', [(t.text, t.ent_type_, t.ent_iob) for t in doc])
 
     # save model to output directory
-    if output_dir is not None:
-        output_dir = Path(output_dir)
-        if not output_dir.exists():
-            output_dir.mkdir()
-        nlp.to_disk(output_dir)
-        print("Saved model to", output_dir)
 
-        # test the saved model
-        print("Loading from", output_dir)
-        nlp2 = spacy.load(output_dir)
-        for text, _ in TRAIN_DATA:
-            doc = nlp2(text)
-            print('Entities', [(ent.text, ent.label_) for ent in doc.ents])
-            print('Tokens', [(t.text, t.ent_type_, t.ent_iob) for t in doc])
+   # test the saved model
+    print("Loading from", output_dir)
+    nlp2 = spacy.load(output_dir)
+    for text, _ in TRAIN_DATA:
+        doc = nlp2(text)
+        print('Entities', [(ent.text, ent.label_) for ent in doc.ents])
+        print('Tokens', [(t.text, t.ent_type_, t.ent_iob) for t in doc])
 
 
 if __name__ == '__main__':
